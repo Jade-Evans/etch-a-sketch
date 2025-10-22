f95a9ca1b1643236a9a8a8ef2c83d97dab66da67
Jade-Evans <jpgabb@msn.com>
2025-10-22 10:00:14 +0100

    feat(gridContainer): assign JS variable reference to HTML div to apply CSS styling and future interactivity

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
+//GLOBAL VARIABLES//
 const gridContainer = document.querySelector(".gridContainer");
-gridContainer.style.width = "560px";
-gridContainer.style.height = "560px";
-gridContainer.style.backgroundColor="blue";
-gridContainer.style.border = "2px solid grey";
-gridContainer.style.margin="auto";
 
-const squareDiv = document.createElement("div");
-squareDiv.classList.add("squareDiv");
-squareDiv.style.backgroundColor = "grey";
-squareDiv.style.border = "1px solid whitesmoke";
-squareDiv.style.maxWidth="100%";
+// gridContainer.style.width = "560px";
+// gridContainer.style.height = "560px";
+// gridContainer.style.backgroundColor="blue";
+// // gridContainer.style.border = "2px solid grey";
+// gridContainer.style.margin="auto";
+// gridContainer.style.display="flex";
+// gridContainer.style.flexWrap="wrap";
 
-let num = 0;
-while (num <= 16){
-    gridContainer.appendChild(squareDiv);
-    num +=1;
-};
+
+
+
+// let num = 0;
+// while (num < 256){
+    
+//     const squareDiv = document.createElement("div");
+//     squareDiv.classList.add("squareDiv");
+//     squareDiv.style.backgroundColor = "grey";
+//     // squareDiv.style.border = "0.2px solid red";
+//     squareDiv.style.boxSizing="borderBox";
+//     squareDiv.style.minWidth="35px";
+//     squareDiv.style.minHSeight="35px";
+//     // squareDiv.style.flex="1 1 1";
+//     gridContainer.appendChild(squareDiv);
+//     num +=1;
+//     console.log(num);
+// };
